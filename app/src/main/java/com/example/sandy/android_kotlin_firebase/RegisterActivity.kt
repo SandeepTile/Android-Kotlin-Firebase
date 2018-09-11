1cd661d6d462934cfa2157603f6eab5c614db2af
sandeeptile <sandeeptile@gmail.com>
2018-09-11 18:48:51 +0530

    google signin button add

--- app/src/main/java/com/example/sandy/android_kotlin_firebase/RegisterActivity.kt
+++ app/src/main/java/com/example/sandy/android_kotlin_firebase/RegisterActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.sandy.android_kotlin_firebase.R.id.regemail
+import com.example.sandy.android_kotlin_firebase.R.id.regpassword
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 //import com.example.sandy.android_kotlin_firebase.R.id.username
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_register.*
 import java.time.Instant
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.tasks.Task
+
 
 //import javax.swing.UIManager.put
 
@@ -22,7 +32,10 @@ import java.time.Instant
 class RegisterActivity : AppCompatActivity() {
 
     private var mAuth: FirebaseAuth? = null
-    private var mDatabase:DatabaseReference?=null
+    lateinit var mDatabase:DatabaseReference
+    lateinit var mGoogleSignInClient:GoogleSignInClient
+    lateinit var gso:GoogleSignInOptions
+    val RC_SIGN_IN:Int=1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,47 +44,79 @@ class RegisterActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        mGoogleSignInClient=GoogleSignIn.getClient(this,gso)
+
 
         regbtn.setOnClickListener {
 
             register()
         }
 
-       /* sign_in_button.setOnClickListener {
 
-            googlesignin()
-        }*/
+        sign_in_button.setOnClickListener {
+
+            googleSignin()
+        }
 
     }
+
+
+
+
+
+    private fun googleSignin() {
+
+
+        val signInIntent:Intent=mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode==RC_SIGN_IN){
+
+            val task:Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult (task)
+        }
+    }
+
+    private fun handleResult(completedTask:Task<GoogleSignInAccount>){
+
+        try {
+
+            val account:GoogleSignInAccount=completedTask.getResult(ApiException::class.java)
+
+
+        }catch (e:ApiException){
+
+            Toast.makeText(this@RegisterActivity,e.toString(),Toast.LENGTH_LONG).show()
+        }
 
     }
 
     private fun register() {
 
 
-            // get the user email and password from the UI
-            // converted the string
-            // here i have created the RXJS Observable to watch the
-            // response from the server
-            // the response is return the 200 i.e OK
-            // then the user is redirected to the admin panel
-
-
         mAuth!!.createUserWithEmailAndPassword(regemail.text.toString(),regpassword.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
 
+                        Toast.makeText(this@RegisterActivity,"enter",Toast.LENGTH_LONG).show()
 
                         val current_user = FirebaseAuth.getInstance().currentUser
                         val uid = current_user!!.uid
 
                         val database = FirebaseDatabase.getInstance().getReference("Users").child(uid)
-                        //val myRef = database.getReference("message")
 
-                       // myRef.setValue("Hello, World!")
 
                         val usermap = HashMap<String,String>()
-                        usermap.put("name", regname.toString())
+                        usermap.put("name", regname.text.toString())
                         usermap.put("status", "Hi there,I'm using Android chatApp")
                         usermap.put("image", "default")
                         usermap.put("thumb_image", "default")
@@ -96,3 +141,7 @@ class RegisterActivity : AppCompatActivity() {
                 }
     }
 }
+
+
+
+
